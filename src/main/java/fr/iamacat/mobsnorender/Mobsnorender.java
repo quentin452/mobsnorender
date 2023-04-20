438e40c286adf67c9527aafa4f07db53e9b7e226
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-04-20 17:18:31 +0200

    tile entity implement test

--- src/main/java/fr/iamacat/mobsnorender/Mobsnorender.java
+++ src/main/java/fr/iamacat/mobsnorender/Mobsnorender.java
@@ -11,15 +11,19 @@ import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import fr.iamacat.mobsnorender.proxy.CommonProxy;
 import fr.iamacat.mobsnorender.utils.Reference;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
+import java.util.logging.Logger;
 
 import java.lang.reflect.Field;
 import java.util.*;
@@ -28,6 +32,8 @@ import net.minecraftforge.common.config.Configuration;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, acceptedMinecraftVersions = Reference.MC_VERSION)
 public class Mobsnorender {
+    private static final Logger LOGGER = Logger.getLogger(Mobsnorender.class.getName());
+
     private final List<String> blacklist = new ArrayList<String>();
 
     private final List<String> tileEntityBlacklist = new ArrayList<String>();
@@ -84,7 +90,7 @@ public class Mobsnorender {
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(this);
     }
-    
+
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
     }
@@ -111,29 +117,41 @@ public class Mobsnorender {
 }
     @SubscribeEvent
     public void onRenderWorldLast(RenderWorldLastEvent event) {
-        // Get the current world
-        World world = Minecraft.getMinecraft().theWorld;
-        Map<TileEntity, TileEntitySpecialRenderer> originalSpecialRenderers = new HashMap<>();
-        for (Object obj : world.loadedTileEntityList) {
-            if (obj instanceof TileEntity) {
-                TileEntity tileEntity = (TileEntity) obj;
-                // Check if the tile entity is associated with a special renderer
-                TileEntitySpecialRenderer renderer = TileEntityRendererDispatcher.instance.getSpecialRenderer(tileEntity);
-                if (renderer != null) {
-                    // Store the original special renderer for the tile entity
-                    originalSpecialRenderers.put(tileEntity, renderer);
-                    // Calculate the X, Y, and Z distance between the entity and the player
-                    double distanceX = Math.abs( tileEntity.xCoord - Minecraft.getMinecraft().thePlayer.posX);
-                    double distanceY = Math.abs(tileEntity.yCoord - Minecraft.getMinecraft().thePlayer.posY);
-                    double distanceZ = Math.abs(tileEntity.zCoord  - Minecraft.getMinecraft().thePlayer.posZ);
-                    // Check if the distance is greater than a certain value
-                    if (distanceX > this.distanceXTileEntity || distanceY > this.distanceYTileEntity || distanceZ > this.distanceZTileEntity) {
-                        // Disable the rendering of the tile entity
-                        renderer = null;
-                    }
+        Minecraft mc = Minecraft.getMinecraft();
+        World world = mc.theWorld;
+        double x = mc.thePlayer.posX;
+        double y = mc.thePlayer.posY;
+        double z = mc.thePlayer.posZ;
+
+        AxisAlignedBB bb = AxisAlignedBB.getBoundingBox(
+                x - distanceXTileEntity,
+                y - distanceYTileEntity,
+                z - distanceZTileEntity,
+                x + distanceXTileEntity + 1,
+                y + distanceYTileEntity + 1,
+                z + distanceZTileEntity + 1
+        );
+
+        // Get all tile entities within the bounding box
+        List<TileEntity> tileEntities = world.getEntitiesWithinAABB(TileEntity.class, bb);
+
+        for (TileEntity tileEntity : tileEntities) {
+            // Check if the tile entity is associated with a special renderer
+            TileEntitySpecialRenderer renderer = TileEntityRendererDispatcher.instance.getSpecialRenderer(tileEntity);
+            if (renderer != null) {
+                // Check if the tile entity should be rendered
+                Vec3 playerPos = Vec3.createVectorHelper(x, y, z);
+                Vec3 tilePos = Vec3.createVectorHelper(tileEntity.xCoord + 0.5, tileEntity.yCoord + 0.5, tileEntity.zCoord + 0.5); // add 0.5 to center the tile entity
+                double distance = playerPos.distanceTo(tilePos);
+                if (distance <= this.distanceXTileEntity && Math.abs(y - tileEntity.yCoord) <= this.distanceYTileEntity && distance <= this.distanceZTileEntity) {
+                    // Render the tile entity
+                    renderer.renderTileEntityAt(tileEntity, tileEntity.xCoord - x, tileEntity.yCoord - y, tileEntity.zCoord - z, event.partialTicks);
+                } else {
+                    // Disable the rendering of the tile entity
+                    TileEntityRendererDispatcher.instance.mapSpecialRenderers.remove(tileEntity.getClass());
+
                 }
+            }
             }
         }
     }
-
-}
